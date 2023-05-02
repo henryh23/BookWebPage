30731eb15f0425503f9b24d9a6f9e16c9ffe2339
atregobov <atregobov@neuralplanet.com>
2023-05-02 18:16:09 -0400

    This has code for sql alchemy connection - need to do a --build because new modeule added to requirements.txt

--- henry_app/app.py
+++ henry_app/app.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+class UnlockedAlchemy(SQLAlchemy):
+    def apply_driver_hacks(self, app, info, options):
+        if "isolation_level" not in options:
+            options["isolation_level"] = "READ COMMITTED"
+        return super(UnlockedAlchemy, self).apply_driver_hacks(app, info, options)
+
 
 app = Flask(__name__)
 mysql = MySQL()
+db = UnlockedAlchemy()
 
 
 if __name__ == '__main__':
@@ -19,7 +30,20 @@ if __name__ == '__main__':
     app.config['MYSQL_PASSWORD'] = 'root'
     app.config['MYSQL_DB'] = 'test'
 
+    db_uri = 'mysql+mysqlconnector://'
+    db_uri += "root" + ':'
+    db_uri += "root" + '@'
+    db_uri += "db" + ':3306/'
+    db_uri += "test"
+
+    # Override finalize so that NUll DB values do not print in jinja as the string "None"
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri  # 'mysql+mysqlconnector://' + root:example@db:3306/carcity'
+    app.config['SQLALCHEMY_POOL_RECYCLE'] = 499
+    app.config['SQLALCHEMY_POOL_TIMEOUT'] = 20
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     mysql.init_app(app)
+    db.init_app(app)
 
     # register blueprints
     from routes import main_blueprint
